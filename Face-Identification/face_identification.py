d4447809dffade7f833299c6b86beb8141edfcbe
Kalinin34 <kmaks5151@inbox.lv>
2024-03-18 10:39:46 +0200

    Lokalais json fails ar nepieciešamo katalogu adresi

--- Face-Identification/face_identification.py
+++ Face-Identification/face_identification.py
@@ -4,25 +4,30 @@ import cv2
 from face_recognition.face_recognition_cli import image_files_in_folder
 
 class FaceIdentification:
-    def __init__(self,photoUrl: str) -> None:
+    #constructor
+    def __init__(self,photoUrl: str,config) -> None:
         self.photo = face_recognition.load_image_file(photoUrl)
-        print("Photo Uploaded!")
+        self.configuration = config
         pass
+    
+    
     def identify(self):
-        ret, photo = cv2.imread("/resources/Steve.jpg")
         face_encoding = face_recognition.face_encodings(self.photo)[0]
 
-        small_frame = cv2.resize(photo, (0, 0), fx=0.25, fy=0.25)
+        small_frame = cv2.resize(None, (0, 0), fx=0.25, fy=0.25)
         rgb_small_frame = small_frame[:, :, ::-1]
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
         #write...
         pass
 class CreatePerson:
+    #constructor
     def __init__(self,datasetUrl,Name) -> None:
         self.folder = datasetUrl
         self.Name = Name
         pass
+    
+    
     def createPerson(self,getPhotoDir,saveDir):
         for images in os.listdir(getPhotoDir):
                 for img_path in image_files_in_folder(os.path.join(getPhotoDir,images)):
--- Face-Identification/test.py
+++ Face-Identification/test.py
@@ -1,12 +1,16 @@
 import cv2
+import json
 from face_identification import FaceIdentification as FC
-from face_identification import CreatePerson as Person
+#from face_identification import CreatePerson as Person
 
 print("Testa Sākums!\n--------------------------------------------------------------")
 print("Git Checking!\n--------------------------------------------------------------")
 print("Git Checking!\n--------------------------------------------------------------2")
 
-steves = Person("resources/dataset_photos/Steve","Steve Jobs")
+config = {}
+with open("..\RTU-Smart-Board\Face-Identification\config.json","r") as jsonFile:
+    config = json.loads(jsonFile.read())
+#steves = Person("resources/dataset_photos/Steve","Steve Jobs")
 tool = FC("/resources/Steve.jpg")
-tool.identify()
+#tool.identify()
 #write...
