82c216d621b90c1e067363a411cb9ee874d8967d
Saphartron <114657109+Saphartron@users.noreply.github.com>
2024-04-15 11:48:27 +0300

    Update Excel_Parse.py

--- Excel/Excel_Parse.py
+++ Excel/Excel_Parse.py
@@ -1,12 +1,47 @@
 import re
+import PyQt5 as pq
+import pandas as pd
 
-cell_value = "Vides un klimata ceļvedis, plkst. 16.30-19.50, 22.02., 29.02., 7.03., 14.03., 21.03. prof. G. Bažbauers Zoom"	
-	
+file_path = 'dszc.dienas.xlsx'
+week = int(input("nedela: "))
 
+if week == 1:
+    sheet_name = '1.nedela'
+elif week == 2:
+    sheet_name = '2.nedela'
+    
 #RE Patterns
-pattern1 = re.compile(r'(?P<subject>.*?)\s*(?P<type>lk\.|pr\.|lb)\s*(?P<teacher>.*?)\s*(?P<place>\d+|Zoom)(?P<extra>.*?)$')
+pattern1 = re.compile(r'(?P<subject>.*?)\s*(?P<type>lk.\.|pr.\.|lb.)\s*(?P<teacher>.*?)\s*(?P<place>\d+|Zoom)(?P<extra>.*?)$')
 pattern2 = re.compile(r'(?P<subject1>.*?),\s*(?P<week1>[\d.-]+\.ned\.)\s*(?P<teacher1>[^,]+),\s*(?P<time1>plkst\.\s*[\d:.]+-[\d:.]+),\s*(?P<place1>.*?)\s*(?P<subject2>.*?),\s*(?P<time2>plkst\.\s*[\d:.]+-[\d:.]+),\s*(?P<teacher2>[^,]+),\s*(?P<week2>.+?),\s*(?P<place2>.+)$')
 pattern3 = re.compile(r'(?P<subject>.*?)(?:plkst\.)(?P<time>.*?)\s*(?:prof\.|lekt\.|doc\.)\s*(?P<teacher>.*?)\s*(?P<place>\d+|Zoom)$')
+
+groups = ["DDBD", "DDBF", "DDBI"]
+
+def Choice (choice, group):
+
+    global cell_value
+    
+    choice = input("Ievadiet grupas nosaukumu: ").upper()
+
+    if choice == "DDBD":
+        cell_value = pd.read_excel(file_path, sheet_name, usecols=[3], nrows=19).iloc[17, 0]	
+
+        if '\n' in cell_value:
+            cell_value = cell_value.replace('\n', ' ')
+
+    elif choice == "DDBF":
+        cell_value = "Inovat. prod. izstrāde un uzņēmējd. [1/1] lk. doc. I. Andersone Zoom"
+    
+    elif choice == "DDBI":
+        cell_value = pd.read_excel(file_path, sheet_name, usecols=[11], nrows=28).iloc[26, 0]
+	
+        if '\n' in cell_value:
+            cell_value = cell_value.replace('\n', ' ')
+
+    else:
+        print("tadas grupas nav")
+
+Choice(None, None)
 
 match1 = pattern1.match(cell_value)
 match2 = pattern2.match(cell_value)
@@ -20,7 +55,7 @@ if match1:  #1st pattern block
     room = match1.group('place').strip()
     extra = match1.group('extra').strip()
 
-    type_mapping = {'lk.': 'Lekcija', 'pr.': 'Praktiska nodarbība', 'lb': 'Laboratorijas darbs'}
+    type_mapping = {'lk.': 'Lekcija', 'pr.': 'Praktiska nodarbība', 'lb.': 'Laboratorijas darbs'}
 
     if room == "Zoom":
         room_output = "Zoom"
@@ -54,7 +89,7 @@ elif match2: #2nd pattern block
         place1 = "Zoom " + place1
         subject2 = subject2[len("Zoom"):].strip()
 
-    print(" 1.\n")
+    print("\n 1.\n")
 
     print(f'Prieksmets: {subject1}')
     print(f'Nedela: {week1}')
