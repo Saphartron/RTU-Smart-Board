495a99473028f597e37ee7e38393fdacdcc67a5c
Villi-ko <64406382+Villi-ko@users.noreply.github.com>
2024-04-17 09:04:11 +0300

    Update Authenticator.py

--- Face_recognition/Authenticator.py
+++ Face_recognition/Authenticator.py
@@ -2,6 +2,7 @@ import dlib
 import cv2
 import json
 import os
+import time
 from scipy.spatial import distance
 
 sp = dlib.shape_predictor('bot/shape_predictor_68_face_landmarks.dat')
@@ -10,7 +11,7 @@ detector = dlib.get_frontal_face_detector()
 face_cascade = cv2.CascadeClassifier('bot/haarcascade_frontalface_default.xml')
 
 def register():
-    def update_json_data(json_data, face_descriptor):
+    def update_json_data(json_data, group, number, name, surname, face_descriptor):
         face_descriptor_list = [value for value in face_descriptor]
         
         data = {
@@ -31,21 +32,37 @@ def register():
 
 
         def det_f(frame):
-            gray = cv2.cvtColor(frame)
-            faces = face_cascade.detectMultiScale(gray, minSize=(30, 30))
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+                        
+            if len(faces) > 0:
+                return True
+            else:
+                return False
                             
         cam = cv2.VideoCapture(0)
         print ("Tiek veikta registrācija")
         screen = None
+        while True:
+            ret, frame = cam.read()
+            if ret:
+                if det_f(frame):
+                    screen = frame.copy()
+                    break
+                else:
+                    print("Jūsu seja nav atpazīta, lūdzu skataties uz kameru!")
+                    time.sleep(1.5)
                     
         dets = detector(screen, 1)
         for k, d in enumerate(dets):
             shape = sp(screen, d)
-            face_descriptor = facerec.compute_face_descriptor(screen)
+            face_descriptor = facerec.compute_face_descriptor(screen, shape)
                             
         cam.release()
         cv2.destroyAllWindows()
                 
+                   
+
         output_dir = ""
         output_file = os.path.join(output_dir, "data.json")
 
@@ -55,12 +72,15 @@ def register():
         else:
             json_data = []
 
-        update_json_data(json_data)
+        update_json_data(json_data, group, number, name, surname, face_descriptor)
 
-        with open(output_file, 'w', encoding='utf-8') as json_file:
+        with open(output_file, 'w') as json_file:
             json.dump(json_data, json_file, indent=4)
         print("Paldies", name, surname, "tu esi registrets!")
                 
+        
+
+
 def authentication():
     with open('data.json', 'r') as file:
         data = json.load(file)
@@ -68,11 +88,26 @@ def authentication():
 
     def detect_faces(frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(150, 150))
+        
+        if len(faces) > 0:
+            return True
+        else:
+            return False
         
     cap = cv2.VideoCapture(0)
     print("Tiek veikta autentifikācija")
     screenshot = None
+
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            if detect_faces(frame):
+                screenshot = frame.copy()
+                break
+            else:
+                print("Jūsu seja nav atpazīta, lūdzu skataties uz kameras pusi!")
+                time.sleep(1.5)
                 
     dets_webcam = detector(screenshot, 1)
             
@@ -82,7 +117,7 @@ def authentication():
 
     for person in data:
         a = distance.euclidean(person['face_features'], face_descriptor2)
-        if a < 0.451:
+        if a < 0.551:
             matchees.append({
                 'group': person["group"],
                 'number': person["number"],
@@ -97,6 +132,9 @@ def authentication():
                 print("Nav atrastas līdzības, meiginiet vēl reiz!")
                 authentication()
             case _:
+                for match in matchees:
+                    print("Grupa: ", match['group'], match['number'])
+                    print("Vārds Uzvārds ", match['name'],match['surname'])
                 cap.release()
                 cv2.destroyAllWindows()
                 print (a)
